be02fbb6eba0617ef504a968169d76d35b3d11d4
Ronin7790 <moresujal26@gmail.com>
2025-09-18 18:37:10 +0530

    Calculator Code that performs basic Arithmetic: addition, subtraction, multiplication, and division.

--- project 1.py
+++ project 1.py
@@ -1,1 +1,34 @@
+def add(x,y):
+    return x+y
 
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    if y==0:
+        return"Error: Division by zero"
+    return x/y
+
+n1 = float(input("Enter first number => "))
+n2 = float(input("Enter second number => "))
+
+op = input("Enter your choice +,-,/,* => ")
+print("<<< Simple Calculator >>>")
+print("Choose Operation:")
+print("1. Add (+)\n2. Subtract (-)\n3. Multiply (*)\n4. Divide (/)")
+
+if op=='+':
+    result=add(n1,n2)
+elif op=='-':
+    result=subtract(n1,n2)
+elif op=='*':
+    result=multiply(n1,n2)
+elif op=='/':
+    result=divide(n1,n2)
+else:
+    result="Invalid Operator"
+
+print("Result:",result)
